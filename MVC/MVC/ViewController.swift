e386763bd8fc34991eba561d2cb5481ad7424a7d
Samith Wijesinghe <samithwijesinha@gmail.com>
2022-12-26 14:52:26 +0530

    MVC design pattern startert project

---
+++ MVC/MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Samith Wijesinghe on 2022-12-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
