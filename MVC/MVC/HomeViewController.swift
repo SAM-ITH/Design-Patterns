3ff507a1d7613423a69eb6cbaaa366ed2379a9a1
Samith <samithwijesinha@gmail.com>
2023-01-10 23:00:58 +0530

    add validation to login fields

--- MVC/MVC/HomeViewController.swift
+++ MVC/MVC/HomeViewController.swift
@@ -11,14 +11,25 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginBTN: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        emailField.addTarget(self, action: #selector(self.validateFields), for: .editingChanged)
+        passwordField.addTarget(self, action: #selector(self.validateFields), for: .editingChanged)
     }
+    
     @IBAction func loginBtnClicked(_ sender: UIButton){
+        loginBTN.isEnabled = emailField.text != "" && passwordField.text != ""
+    }
+    
+    @objc private func validateFields() {
         
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
