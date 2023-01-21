c723421cba2a6d6b30f61198d9bc0ecb1010ddc6
Samith <samithwijesinha@gmail.com>
2023-01-22 04:05:24 +0530

    add storyboard identifier

--- MVC/MVC/LoginViewController.swift
+++ MVC/MVC/LoginViewController.swift
@@ -10,22 +10,16 @@ import UIKit
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var WelComeLbl: UILabel!
+    var user: User!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        user = NetworkService.shared.getLoggedInUser()
+        welcomeUser()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func welcomeUser(){
+        WelComeLbl.text = "Hi, \(user.firstName) \(user.lastName)"
     }
-    */
-
 }
