e6d2b81b762673abd7b4edcea54af63ba8e7535a
Samith <samithwijesinha@gmail.com>
2023-01-28 11:40:28 +0530

    creating the interfaces

---
+++ MVVM/MVVM/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  MVVM
+//
+//  Created by Samith on 2023-01-28.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ MVVM/MVVM/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Samith on 2023-01-28.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
