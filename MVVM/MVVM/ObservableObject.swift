189ac337a018922d3e09cbce90738dc99626e458
Samith <samithwijesinha@gmail.com>
2023-02-01 08:01:08 +0530

    finalise the observable object class

--- MVVM/MVVM/ObservableObject.swift
+++ MVVM/MVVM/ObservableObject.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
+class observableObject<T>
+{
+    
+    var value: T?
+    
+    private var listener: ((T?) -> Void)? // in here this use an a single object but we can adjust it to used as array that multiple objects can subcribe. 
+    
+    init(_ value: T?)
+    {
+        self.value = value
+    }
+    
+    func bind(_ listener: @escaping(T?) -> Void)
+    {
+        listener(value)
+        self.listener = listener
+    }
+}
 
