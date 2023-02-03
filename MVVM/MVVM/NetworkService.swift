6e73b092f29817c8502571dda5bff836cc1fd220
Samith <samithwijesinha@gmail.com>
2023-02-03 21:37:53 +0530

    implement the network class

--- MVVM/MVVM/NetworkService.swift
+++ MVVM/MVVM/NetworkService.swift
@@ -8,3 +8,24 @@
 import Foundation
 
 // this class simulate the backend funtionality
+final class NetworkService {
+    static let shared = NetworkService() // singelton
+    
+    var user: User?
+    
+    private init() { }
+    
+    func login(email: String, password: String, completion: @escaping(Bool) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            [weak self] in
+            if email == "test@test.com" && password == "password"
+            {
+                self?.user = User(firstName: "Samith", lastName: "wijesinghe", email: "test@test.com", age: 21)
+                completion(true)
+            } else {
+                self?.user = nil
+                completion(false)
+            }
+        }
+    }
+}
