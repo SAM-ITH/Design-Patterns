4d8d2229b939e0317fcfd53c2d77c319dd212311
Samith <samithwijesinha@gmail.com>
2023-02-03 22:29:44 +0530

    setup binders

--- MVVM/MVVM/LoginView/LoginViewController.swift
+++ MVVM/MVVM/LoginView/LoginViewController.swift
@@ -19,10 +19,32 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupBinders()
+        
+    }
+    
+    private func setupBinders() {
+        viewmodel.error.bind { [weak self] error in
+            if let error = error {
+                print(error)
+            } else {
+                self?.goToHomePage()
+            }
+        }
     }
 
+    private func goToHomePage() {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        present(controller, animated: true, completion: nil)
+    }
+    
     @IBAction func loginBtnClicked(_ sender: Any) {
-        
+        guard let email = emailField.text,
+              let password = passwordField.text else {
+            print("please enter an email and password")
+            return
+        }
+        viewmodel.login(email: email, password: password)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- MVVM/MVVM/ObservableObject.swift
+++ MVVM/MVVM/ObservableObject.swift
@@ -10,20 +10,20 @@ import Foundation
 class observableObject<T>
 {
     // listner object
-    var value: T? {
+    var value: T {
         didSet{
             listener?(value)
         }
     }
     
-    private var listener: ((T?) -> Void)? // in here this use an a single object but we can adjust it to used as array that multiple objects can subcribe. 
+    private var listener: ((T) -> Void)? // in here this use an a single object but we can adjust it to used as array that multiple objects can subcribe.
     
-    init(_ value: T?)
+    init(_ value: T)
     {
         self.value = value
     }
     
-    func bind(_ listener: @escaping(T?) -> Void)
+    func bind(_ listener: @escaping(T) -> Void)
     {
         listener(value)
         self.listener = listener
