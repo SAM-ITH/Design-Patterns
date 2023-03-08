018586e493407dac85a5d7985949d6cf2b88df72
Samith Wijesinghe <samith.w@incentivio.com>
2023-03-08 14:17:46 +0530

    changes to the file structure of the project

---
+++ Objc_MVC/Objc_MVC/Model/User.h
@@ -0,0 +1,12 @@
+//
+//  User.h
+//  Objc_MVC
+//
+//  Created by Samith Wijesinghe on 2023-03-08.
+//
+
+#ifndef User_h
+#define User_h
+
+
+#endif /* User_h */
---
+++ Objc_MVC/Objc_MVC/Model/User.m
@@ -0,0 +1,8 @@
+//
+//  User.m
+//  Objc_MVC
+//
+//  Created by Samith Wijesinghe on 2023-03-08.
+//
+
+#import <Foundation/Foundation.h>
